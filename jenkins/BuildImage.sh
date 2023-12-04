cd685a92367d2af37563b3ecd7f7628d8b9e2006
sOmEoNe2lOvE <raymcraft@gmail.com>
2023-12-04 16:53:33 +0100

    Add Jenkins configuration files and Docker build script

---
+++ jenkins/BuildImage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# To be executed in parent directory
+# Builds custom Jenkins image for the WhanOS project.
+
+docker build -t jenkins:local -f jenkins/Dockerfile .
--- jenkins/stop_jenkins.sh
+++
@@ -1,5 +0,0 @@
-# #!/bin/bash
-# To be executed anywhere
-# Stops Jenkins Docker container
-
-docker stop jenkins
